80129a05e774ce989a61c204258a413901777b7e
li-juncai <1844287927@qq.com>
2025-11-28 10:29:39 +0800

    Update main.tf

--- environments/production/main.tf
+++ environments/production/main.tf
@@ -1,4 +1,15 @@
+
+terraform {
+  backend "remote" {
+    # 同测试环境后端配置
+  }
+}
+
 module "prod_role" {
-  source     = "../../modules/role"
-  role_name  = "prod-role-${random_string.suffix.result}"
+  source = "../../modules/alicloud-role"
+  # 可添加正式环境特定参数
+}
+
+output "prod_role_arn" {
+  value = module.prod_role.role_arn
 }
