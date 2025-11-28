d3324aead5647f664fdf14ceb8490c5f329262f7
li-juncai <1844287927@qq.com>
2025-11-28 10:05:58 +0800

    Update main.tf

--- modules/role/main.tf
+++ modules/role/main.tf
@@ -1,1 +1,21 @@
 
+resource "alicloud_ram_role" "new_role" {
+  name        = var.role_name
+  description  = "Terraform-managed role with no permissions"
+  force       = true
+}
+
+resource "alicloud_ram_policy" "empty_policy" {
+  name        = "${var.role_name}-empty-policy"
+  description  = "Empty policy for initial role"
+  policy_document = jsonencode({
+    Version   = "1"
+    Statement = []
+  })
+}
+
+resource "alicloud_ram_role_policy_attachment" "attach_empty" {
+  role_name   = alicloud_ram_role.new_role.name
+  policy_name = alicloud_ram_policy.empty_policy.name
+  policy_type = "Custom"
+}
