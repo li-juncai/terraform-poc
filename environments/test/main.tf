09a6a3e9fd28954c214b2106b021cf0c6615f7e7
li-juncai <1844287927@qq.com>
2025-11-28 11:15:05 +0800

    Update main.tf

--- environments/test/main.tf
+++ environments/test/main.tf
@@ -1,5 +1,16 @@
-# environments/test/main.tf
+terraform {
+  backend "remote" {
+    # 动态加载backend.hcl配置
+    # 实际需通过-backend-config=../backend-config/backend.hcl传递
+  }
+}
+
+
 module "alicloud_role" {
   source = "../../modules/alicloud-role"
   service = "ecs.aliyuncs.com"  # 修改测试环境角色信任的服务
+}
+
+output "test_role_arn" {
+  value = module.test_role.role_arn
 }
